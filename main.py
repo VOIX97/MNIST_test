264c00d9d222f841b7d17ccdfe2c8fad604b16d0
VOIX97 <xiangsheng1325@sjtu.edu.cn>
2019-03-26 12:20:08 +0800

    commit homework file

---
+++ main.py
@@ -0,0 +1,26 @@
+#! /user/bin/env python
+
+from load_samples import *
+from NeuralNet import *
+import numpy as np
+import random
+import os, struct
+from array import array as pyarray
+from numpy import append, array, int8, uint8, zeros
+
+if __name__ == '__main__':
+  INPUT = 28*28
+  OUTPUT = 10
+  net = NeuralNet([INPUT, 40, OUTPUT])
+
+  train_set = load_samples(dataset='training_data')
+  test_set = load_samples(dataset='testing_data')
+
+  net.SGD(train_set, 13, 100, 3.0, test_data=test_set)
+  
+  # accuracy
+  correct = 0;
+  for test_feature in test_set:
+    if net.predict(test_feature[0]) == test_feature[1][0]:
+      correct += 1
+  print("准确率: ", correct/len(test_set))
